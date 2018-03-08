9a0ec584cba434fae1049a5dc3cacceea13a031f
Akshay Mittal <mittalakshay6@gmail.com>
2018-03-08 14:01:33 +0530

    mainActivity removed, ready for further work

--- app/src/main/java/com/example/akshay/attendencebarcode/MainActivity.java
+++ app/src/main/java/com/example/akshay/attendencebarcode/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.akshay.attendencebarcode;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView mConnectionStatusView = findViewById(R.id.connectionStatus);
+        Button mInitiateConnectionBtn = findViewById(R.id.initiateConnectionBtn);
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/akshay/attendencebarcode/clientServer/Connection.java
@@ -0,0 +1,5 @@
+package com.example.akshay.attendencebarcode.clientServer;
+
+public class Connection {
+
+}
