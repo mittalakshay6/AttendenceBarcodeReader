9f2cdc79140be07179f333543f25bd3fcd322d82
Akshay Mittal <mittalakshay6@gmail.com>
2018-03-10 20:02:07 +0530

    Main Activity revamped. needed views added.

--- app/src/main/java/com/example/akshay/attendencebarcode/MainActivity.java
+++ app/src/main/java/com/example/akshay/attendencebarcode/MainActivity.java
@@ -1,21 +1,81 @@
 package com.example.akshay.attendencebarcode;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.akshay.attendencebarcode.clientServer.ConnectionClient;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.vision.barcode.Barcode;
+
+import org.w3c.dom.Text;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    TextView regNView;
+    EditText ipAddrText;
+    EditText confirmView;
+
+    private static final int RC_BARCODE_CAPTURE = 9001;
+    private static final String TAG = "BarcodeMain";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView mConnectionStatusView = findViewById(R.id.connectionStatus);
-        Button mInitiateConnectionBtn = findViewById(R.id.initiateConnectionBtn);
+        regNView = findViewById(R.id.regNView);
+        ipAddrText = findViewById(R.id.ipAddrText);
+        confirmView = findViewById(R.id.confirmView);
+
+        findViewById(R.id.startCameraBtn).setOnClickListener(this);
+    }
 
 
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.startCameraBtn) {
+            // launch barcode activity.
+            Intent intent = new Intent(this, BarcodeCaptureActivity.class);
+            intent.putExtra(BarcodeCaptureActivity.AutoFocus, true);
+            intent.putExtra(BarcodeCaptureActivity.UseFlash, false);
 
+            startActivityForResult(intent, RC_BARCODE_CAPTURE);
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RC_BARCODE_CAPTURE) {
+            if (resultCode == CommonStatusCodes.SUCCESS) {
+                if (data != null) {
+                    Barcode barcode = data.getParcelableExtra(BarcodeCaptureActivity.BarcodeObject);
+//                    statusMessage.setText(R.string.barcode_success);
+                    regNView.setVisibility(View.VISIBLE);
+                    regNView.setText(barcode.displayValue);
+                    confirmView.setVisibility(View.VISIBLE);
+                    Log.d(TAG, "Barcode read: " + barcode.displayValue);
+                } else {
+//                    statusMessage.setText(R.string.barcode_failure);
+                    Log.d(TAG, "No barcode captured, intent data is null");
+                }
+            } else {
+//                statusMessage.setText(String.format(getString(R.string.barcode_error),
+//                        CommonStatusCodes.getStatusCodeString(resultCode)));
+                Log.d(TAG, "There is Some error");
+            }
+        }
+        else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
--- app/src/main/java/com/example/akshay/attendencebarcode/clientServer/Connection.java
+++
@@ -1,5 +0,0 @@
-package com.example.akshay.attendencebarcode.clientServer;
-
-public class Connection {
-
-}
